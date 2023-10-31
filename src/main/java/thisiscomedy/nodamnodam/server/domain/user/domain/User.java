67f8441b800b89092f63081ee0b60ece58839e55
마현우 <mhw35689402@gmail.com>
2023-11-01 08:22:37 +0900

    add: User 엔티티 추가

---
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/user/domain/User.java
@@ -0,0 +1,33 @@
+package thisiscomedy.nodamnodam.server.domain.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import thisiscomedy.nodamnodam.server.global.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer smokePerDay;
+
+    @Column(nullable = false)
+    private Integer cigarettePrice;
+
+    @Builder
+    public User(String name, Integer smokePerDay, Integer cigarettePrice) {
+        this.name = name;
+        this.smokePerDay = smokePerDay;
+        this.cigarettePrice = cigarettePrice;
+    }
+}
