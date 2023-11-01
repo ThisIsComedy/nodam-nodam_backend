a874458eb172fbfc001c8adbbb0dc05a512708d7
마현우 <mhw35689402@gmail.com>
2023-11-01 09:01:58 +0900

    add: user repository

---
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package thisiscomedy.nodamnodam.server.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import thisiscomedy.nodamnodam.server.domain.user.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
