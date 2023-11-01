cbf8afdd138a4e810cab1c72cb212a8079208b58
마현우 <mhw35689402@gmail.com>
2023-11-02 08:55:20 +0900

    add: openfeign 도입

--- src/main/java/thisiscomedy/nodamnodam/server/ServerApplication.java
+++ src/main/java/thisiscomedy/nodamnodam/server/ServerApplication.java
@@ -3,11 +3,13 @@ package thisiscomedy.nodamnodam.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class ServerApplication {
 
     public static void main(String[] args) {
