8a4d90a7708e7fbd75fc6fca520fb934050d1164
마현우 <mhw35689402@gmail.com>
2023-11-06 20:02:09 +0900

    add: 로그아웃 기능 추가

---
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/auth/application/UserLogoutService.java
@@ -0,0 +1,24 @@
+package thisiscomedy.nodamnodam.server.domain.auth.application;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import thisiscomedy.nodamnodam.server.global.jwt.dto.TokenResponse;
+
+@Service
+@RequiredArgsConstructor
+public class UserLogoutService {
+
+    private final RefreshTokenSaveService refreshTokenSaveService;
+
+    public String execute(HttpServletRequest request) {
+        String accessToken = request.getHeader("Authorization").split(" ")[1].trim();
+        String refreshToken = request.getHeader("Authorization-refresh").split(" ")[1].trim();
+
+        refreshTokenSaveService.execute(
+                new TokenResponse(accessToken, refreshToken)
+        );
+
+        return "SUCCESS";
+    }
+}
