8815a90f4be96f45613c4d6fb4ba02e29b023c05
마현우 <mhw35689402@gmail.com>
2023-11-07 01:23:59 +0900

    refactor: npe 예외처리

--- src/main/java/thisiscomedy/nodamnodam/server/domain/auth/application/UserLogoutService.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/auth/application/UserLogoutService.java
@@ -3,6 +3,7 @@ package thisiscomedy.nodamnodam.server.domain.auth.application;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import thisiscomedy.nodamnodam.server.domain.user.exception.BadRequestException;
 import thisiscomedy.nodamnodam.server.global.jwt.dto.TokenResponse;
 
 @Service
@@ -12,13 +13,17 @@ public class UserLogoutService {
     private final RefreshTokenSaveService refreshTokenSaveService;
 
     public String execute(HttpServletRequest request) {
-        String accessToken = request.getHeader("Authorization").split(" ")[1].trim();
-        String refreshToken = request.getHeader("Authorization-refresh").split(" ")[1].trim();
+        try {
+            String accessToken = request.getHeader("Authorization").split(" ")[1].trim();
+            String refreshToken = request.getHeader("Authorization-refresh").split(" ")[1].trim();
 
-        refreshTokenSaveService.execute(
-                new TokenResponse(accessToken, refreshToken)
-        );
+            refreshTokenSaveService.execute(
+                    new TokenResponse(accessToken, refreshToken)
+            );
 
-        return "SUCCESS";
+            return "SUCCESS";
+        } catch (NullPointerException e) {
+            throw BadRequestException.EXCEPTION;
+        }
     }
 }
