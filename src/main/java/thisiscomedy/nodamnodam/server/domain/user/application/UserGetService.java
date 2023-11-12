4b373f745c34a4d3a9ced9a8cba7e3504669bba8
마현우 <mhw35689402@gmail.com>
2023-11-12 14:43:55 +0900

    refactor: 순환참조 예방을 위한 프로필 가져오기 메서드 분리

--- src/main/java/thisiscomedy/nodamnodam/server/domain/user/application/UserGetService.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/user/application/UserGetService.java
@@ -3,11 +3,8 @@ package thisiscomedy.nodamnodam.server.domain.user.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import thisiscomedy.nodamnodam.server.domain.stats.application.StatsGetService;
-import thisiscomedy.nodamnodam.server.domain.stats.presentation.dto.response.StatsGetBadgeResponse;
 import thisiscomedy.nodamnodam.server.domain.user.domain.User;
 import thisiscomedy.nodamnodam.server.domain.user.exception.UserNotFoundException;
-import thisiscomedy.nodamnodam.server.domain.user.presentation.dto.response.UserGetProfileResponse;
 import thisiscomedy.nodamnodam.server.domain.user.repository.UserRepository;
 
 @Service
@@ -15,7 +12,6 @@ import thisiscomedy.nodamnodam.server.domain.user.repository.UserRepository;
 public class UserGetService {
 
     private final UserRepository userRepository;
-    private final StatsGetService statsGetService;
 
     public User getUser() {
         Long userId = Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
@@ -30,24 +26,5 @@ public class UserGetService {
     public User findById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-    }
-
-    public UserGetProfileResponse getProfile() {
-        Long userId = Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
-
-        StatsGetBadgeResponse badgeInfo = statsGetService.getBadge(user);
-
-        return new UserGetProfileResponse(
-                user.getName(),
-                user.getEmail(),
-                user.getProfileUrl(),
-                badgeInfo.getNoSmokeDay(),
-                badgeInfo.getMaximumContinuityNoSmoke(),
-                badgeInfo.getSaveMoney(),
-                badgeInfo.getThreeDayContinuityNoSmoke()
-        );
     }
 }
