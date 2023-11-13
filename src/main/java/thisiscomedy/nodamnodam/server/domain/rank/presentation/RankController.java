2c83303a0808a976490fd3334eb03996ef2d0199
마현우 <mhw35689402@gmail.com>
2023-11-14 01:17:21 +0900

    add: 랭크 등록 여부 상태 변경하는 기능 추가

--- src/main/java/thisiscomedy/nodamnodam/server/domain/rank/presentation/RankController.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/rank/presentation/RankController.java
@@ -1,9 +1,21 @@
 package thisiscomedy.nodamnodam.server.domain.rank.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import thisiscomedy.nodamnodam.server.domain.rank.application.RankStateUpdateService;
 
 @RestController
 @RequestMapping("/api/rank")
+@RequiredArgsConstructor
 public class RankController {
+
+    private final RankStateUpdateService rankStateUpdateService;
+
+    @PostMapping
+    public ResponseEntity<Long> updateRankState() {
+        return ResponseEntity.ok(rankStateUpdateService.execute());
+    }
 }
