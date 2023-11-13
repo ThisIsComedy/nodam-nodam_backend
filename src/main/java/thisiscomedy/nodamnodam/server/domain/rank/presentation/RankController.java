0e54407f9b15448db0d5c22791a203bba535e878
마현우 <mhw35689402@gmail.com>
2023-11-14 08:25:07 +0900

    add: 랭크 데이터 가져오기 기능 추가

--- src/main/java/thisiscomedy/nodamnodam/server/domain/rank/presentation/RankController.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/rank/presentation/RankController.java
@@ -2,17 +2,28 @@ package thisiscomedy.nodamnodam.server.domain.rank.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import thisiscomedy.nodamnodam.server.domain.rank.application.RankGetService;
 import thisiscomedy.nodamnodam.server.domain.rank.application.RankStateUpdateService;
+import thisiscomedy.nodamnodam.server.domain.rank.presentation.dto.response.RankResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rank")
 @RequiredArgsConstructor
 public class RankController {
 
+    private final RankGetService rankGetService;
     private final RankStateUpdateService rankStateUpdateService;
+
+    @GetMapping
+    public ResponseEntity<List<RankResponse>> getRank() {
+        return ResponseEntity.ok(rankGetService.execute());
+    }
 
     @PostMapping
     public ResponseEntity<Long> updateRankState() {
---
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/rank/presentation/dto/response/RankResponse.java
@@ -0,0 +1,8 @@
+package thisiscomedy.nodamnodam.server.domain.rank.presentation.dto.response;
+
+public record RankResponse(
+        String userName,
+        String profileImage,
+        int threeDayContinuityNoSmoke
+) {
+}
