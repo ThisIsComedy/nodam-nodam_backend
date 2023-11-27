9847c49a90a8668a3c48aee5b6f829548e4c56e8
마현우 <mhw35689402@gmail.com>
2023-11-27 15:46:08 +0900

    refactor: 잔디 응답 dto record로 리팩토링

--- src/main/java/thisiscomedy/nodamnodam/server/domain/smoke/presentation/dto/response/GrassGetResponse.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/smoke/presentation/dto/response/GrassGetResponse.java
@@ -1,16 +1,13 @@
 package thisiscomedy.nodamnodam.server.domain.smoke.presentation.dto.response;
 
-import lombok.Getter;
 import thisiscomedy.nodamnodam.server.domain.smoke.domain.Smoke;
 
 import java.time.LocalDate;
 
-@Getter
-public class GrassGetResponse {
-
-    private final LocalDate at;
-
+public record GrassGetResponse(
+        LocalDate at
+) {
     public GrassGetResponse(Smoke smoke) {
-        this.at = smoke.getCreatedAt();
+        this(smoke.getCreatedAt());
     }
 }
