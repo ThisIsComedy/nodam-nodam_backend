656b8a8c520c83ba2be88cf9314e8f405231ed8b
마현우 <mhw35689402@gmail.com>
2023-11-27 16:48:36 +0900

    refactor: 잔디 가져오기 응답 형식 변경

--- src/main/java/thisiscomedy/nodamnodam/server/domain/smoke/presentation/dto/response/GrassGetResponse.java
+++ src/main/java/thisiscomedy/nodamnodam/server/domain/smoke/presentation/dto/response/GrassGetResponse.java
@@ -1,13 +1,9 @@
 package thisiscomedy.nodamnodam.server.domain.smoke.presentation.dto.response;
 
-import thisiscomedy.nodamnodam.server.domain.smoke.domain.Smoke;
-
 import java.time.LocalDate;
+import java.util.List;
 
 public record GrassGetResponse(
-        LocalDate at
-) {
-    public GrassGetResponse(Smoke smoke) {
-        this(smoke.getCreatedAt());
-    }
-}
+        LocalDate userCreatedAt,
+        List<UserSmokedAtResponse> userSmokedAtResponses
+) {}
